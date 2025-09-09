e3ed666e5ff9ef89cfeff461bb7e32c1f330d53a
benchuser <benchuser@vmi2592346.contaboserver.net>
2025-09-09 20:11:47 +0200

    Sales Order Analysis

--- mdpl_custom_report/mdpl_custom_report/raven_functions.py
+++ mdpl_custom_report/mdpl_custom_report/raven_functions.py
@@ -1,6 +1,17 @@
 import frappe
 from frappe.utils import nowdate
 import json
+from datetime import date
+
+def convert_dates(obj):
+    if isinstance(obj, dict):
+        return {k: convert_dates(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [convert_dates(i) for i in obj]
+    elif isinstance(obj, date):
+        return obj.isoformat()
+    else:
+        return obj
 
 @frappe.whitelist()
 def generate_sales_order_analysis(from_date=None, to_date=None):
@@ -16,15 +27,19 @@ def generate_sales_order_analysis(from_date=None, to_date=None):
         report = frappe.get_doc("Report", report_name)
         columns, data = report.get_data(filters=filters, as_dict=True)
 
+        # Convert dates to strings before logging
+        safe_columns = convert_dates(columns)
+        safe_data = convert_dates(data)
+
         frappe.log_error(json.dumps({
-            "columns": columns,
-            "data": data
+            "columns": safe_columns,
+            "data": safe_data
         }), "Sales Order Report Debug")
 
         return {
             "message": f"Generate Sales Order Analysis report from {from_date} to {to_date}",
-            "columns": columns,
-            "data": data
+            "columns": safe_columns,
+            "data": safe_data
         }
 
     except Exception as e:
